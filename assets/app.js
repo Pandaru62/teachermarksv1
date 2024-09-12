155276d623025e25067cfd03e05cd01fa66a657d
Loris Pandaru <loris.buchelet@gmail.com>
2024-09-12 16:16:26 +0200

    solving chart pb

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,25 @@
 import './bootstrap.js';
 
 import './styles/app.css';
+
+// script associated to showstudent.html.twig
+import { Chart, registerables } from 'chart.js';
+
+Chart.register(...registerables);
+
+document.addEventListener('DOMContentLoaded', function () {
+    const chartElement = document.getElementById('myChart');
+    if (chartElement) {
+        const ctx = chartElement.getContext('2d');
+        const chartData = JSON.parse(document.getElementById('chartData').textContent);
+        const chartOptions = JSON.parse(document.getElementById('chartOptions').textContent);
+        
+        new Chart(ctx, {
+            type: 'radar',
+            data: chartData,
+            options: chartOptions
+        });
+    } else {
+        console.error('Chart element not found');
+        }
+});
